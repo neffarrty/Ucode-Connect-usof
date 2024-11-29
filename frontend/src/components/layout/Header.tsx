678bf9409af80354eaf2281f3500605adf0256f2
neffarrty <egorkovtun8@gmail.com>
2024-11-30 00:37:05 +0200

    feat: add menu to header avatar

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Box, AppBar, Toolbar, Typography, Avatar, Badge } from '@mui/material';
+import {
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Avatar,
+	Menu,
+	MenuItem,
+	IconButton,
+} from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import logo from '../../assets/header-logo.svg';
+import { AccountCircle, ExitToApp } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 export const Header: React.FC = () => {
 	const { user } = useSelector((state: RootState) => state.auth);
+	const navigate = useNavigate();
+	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+		null,
+	);
+
+	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElUser(event.currentTarget);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
 	return (
 		<Box>
@@ -15,12 +37,7 @@ export const Header: React.FC = () => {
 				sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
 			>
 				<Toolbar>
-					<Box
-						sx={{
-							display: 'flex',
-							flexGrow: 1,
-						}}
-					>
+					<Box sx={{ display: 'flex', flexGrow: 1 }}>
 						<Box component="a" href="/">
 							<Box
 								component="img"
@@ -33,17 +50,46 @@ export const Header: React.FC = () => {
 						<Typography
 							variant="subtitle1"
 							component="span"
-							sx={{
-								flexGrow: 1,
-							}}
+							sx={{ flexGrow: 1 }}
 						>
 							{user?.login}
 						</Typography>
-						<Avatar
-							alt={user?.login}
-							src={user?.avatar}
-							sx={{ border: '2px solid #fff' }}
-						/>
+						<IconButton onClick={handleOpenUserMenu}>
+							<Avatar
+								alt={user?.login}
+								src={user?.avatar}
+								sx={{ border: '2px solid #fff' }}
+							/>
+						</IconButton>
+						<Menu
+							sx={{ mt: '45px' }}
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'right',
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							<MenuItem onClick={() => navigate('/profile')}>
+								<AccountCircle sx={{ mr: 1 }} />
+								<Typography sx={{ textAlign: 'center' }}>
+									Profile
+								</Typography>
+							</MenuItem>
+							<MenuItem href="/profile">
+								<ExitToApp
+									sx={{ mr: 1, color: 'primary.dark' }}
+								/>
+								<Typography sx={{ textAlign: 'center' }}>
+									Log Out
+								</Typography>
+							</MenuItem>
+						</Menu>
 					</Box>
 				</Toolbar>
 			</AppBar>
